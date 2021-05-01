d1e8d7554497be9205fa0cd6b741818c3850f67e
dtran22 <trantdu4164@gmail.com>
2021-04-30 22:02:25 -0400

    Create SWE432assignment8.js

---
+++ src/components/SWE432assignment8.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function SWE432assignment8(props) {
+  const [inputValue, result] = useState(null);
+  const [print, setPrint] = useState(false);
+
+  const randomize = (event) => {
+    var array = event.target.value.split(',');
+    var name = array[Math.floor(Math.random() * array.length)];
+    result(name)
+  }
+  return (
+    <div>
+      <h2>SWE 432 Assignment 8</h2>
+      <table>
+        <tr>
+          <td>Names:</td>
+          <td>
+            <input type="text" onChange={randomize}/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button onClick={()=>setPrint(true)}>Click Me!</button>
+          </td>
+        </tr>
+      </table>
+      {
+        print?
+        <h1>{inputValue}</h1>
+        :null
+      }
+    </div>
+  )
+}
+
+export default SWE432assignment8;
